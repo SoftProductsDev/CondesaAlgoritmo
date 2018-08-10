87cd6d223f3ae6dbd76276b4d2a4f9a233e9abe2
javier <javier@FVACAGTO>
2018-08-09 22:45:16 -0500

    GUI improvements

--- src/condesaGUI/AddGUI.java
+++ src/condesaGUI/AddGUI.java
@@ -4,11 +4,11 @@ import condeso.Condeso;
 import condeso.Contrato;
 import condeso.TipoEmpleado;
 import java.util.ArrayList;
-import java.util.List;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Font;
 import tiendas.Tiendas;
-import java.util.Set;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -36,6 +36,8 @@ public class AddGUI extends Application{
   private ChoiceBox<Contrato> contratoCh;
   private RadioButton cajaTS;
   private RadioButton vespertinoCh;
+  private RadioButton temporal;
+  private Label errorMssg;
 
 
   @Override
@@ -46,32 +48,98 @@ public class AddGUI extends Application{
 
       @Override
       public void handle(ActionEvent event) {
+        if(CheckValidInput()){
         int level = 0;
-        try {level = Integer.parseInt(levelTF.getText());}
-        catch(Exception e){String message = "Ingrese un número";}
-
-        Condeso condeso = new Condeso(
-            empleoCh.getValue(),
-            nameTF.getText(),
-            fijoCh.isSelected(),
-            level,
-            matutinoCh.isSelected(),
-            cajaTS.isSelected(),
-            fechaContrataciónDP.getValue(),
-            new ArrayList<Tiendas>(),
-            Contrato.Tipo1
-        );
-
-        System.out.println(condeso);
+        try {
+          level = Integer.parseInt(levelTF.getText());
+          Condeso condeso = CreateCondeso(level);
+          errorMssg.setText("Se ha añadido: " + condeso.getNombre());
+          System.out.println(condeso);
+        }
+        catch(Exception e)
+        {
+          errorMssg.setText("Ingrese un valor núemrico en nivel");
+        }
+        }
       }
     });
 
+    CreateControls();
+
+    TilePane tile = new TilePane(Orientation.VERTICAL);
+    tile.setPadding(new Insets(5, 0, 5, 0));
+    tile.setVgap(8);
+    tile.setHgap(4);
+    tile.setPrefColumns(2);
+    tile.setStyle("-fx-background-color: DAE6F3;");
+    tile.setAlignment(Pos.BASELINE_CENTER);
+
+    tile.getChildren().add(new Label("Nombre del empleado"));
+    tile.getChildren().add(nameTF);
+    tile.getChildren().add(new Label("Seleccione el cargo"));
+    tile.getChildren().add(empleoCh);
+    tile.getChildren().add(new Label("Nivel - Escriba un número"));
+    tile.getChildren().add(levelTF);
+    tile.getChildren().add(new Label("Elija si es un trabajador fijo o temporal"));
+    tile.getChildren().add(fijoCh);
+    tile.getChildren().add(temporal);
+    tile.getChildren().add(new Label("Seleccione si tiene permisos de usar la caja"));
+    tile.getChildren().add(cajaTS);
+    tile.getChildren().add(new Label("Seleccione el tipo de turno"));
+    tile.getChildren().add(matutinoCh);
+    tile.getChildren().add(vespertinoCh);
+    tile.getChildren().add(new Label("Fecha de contratación (DD/MM/YYYY)"));
+    tile.getChildren().add(fechaContrataciónDP);
+    tile.getChildren().add(new Label("Seleccione el tipo de contrato"));
+    tile.getChildren().add(contratoCh);
+    tile.getChildren().add(btn);
+    tile.getChildren().add(errorMssg);
+
+    Scene scene = new Scene(tile, 300, 700);
+
+    primaryStage.setTitle("Añadir Condesos");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  private boolean CheckValidInput()
+  {
+    if(!nameTF.getText().isEmpty() &&
+        empleoCh.getValue() != null &&
+        !levelTF.getText().isEmpty()&&
+        fijoCh.isSelected() || temporal.isSelected() &&
+        matutinoCh.isSelected() || vespertinoCh.isSelected()
+        && fechaContrataciónDP.getValue() != null
+        && contratoCh.getValue() != null){return true;}
+        else
+          {
+            errorMssg.setText("Hay campos incompletos, inténtelo de nuevo");
+            return false;
+          }
+  }
+
+  private Condeso CreateCondeso(int level) {
+    Condeso condeso = new Condeso(
+        empleoCh.getValue(),
+        nameTF.getText(),
+        fijoCh.isSelected(),
+        level,
+        matutinoCh.isSelected(),
+        cajaTS.isSelected(),
+        fechaContrataciónDP.getValue(),
+        new ArrayList<Tiendas>(),
+        Contrato.Tipo1
+    );
+    return condeso;
+  }
+
+  private void CreateControls(){
     ToggleGroup group1 = new ToggleGroup();
     fijoCh = new RadioButton("Fijo");
     fijoCh.setToggleGroup(group1);
-    RadioButton temporal = new RadioButton("Temporal");
+    temporal = new RadioButton("Temporal");
     temporal.setToggleGroup(group1);
-    nameTF = new TextField("Nombre");
+    nameTF = new TextField();
     empleoCh = new ChoiceBox<TipoEmpleado>
         (FXCollections.observableArrayList(TipoEmpleado.Encargado,
             TipoEmpleado.GM, TipoEmpleado.Equipo, TipoEmpleado.Nuevo));
@@ -83,35 +151,11 @@ public class AddGUI extends Application{
     fechaContrataciónDP = new DatePicker();
     contratoCh = new ChoiceBox<Contrato>
         (FXCollections.observableArrayList(Contrato.Tipo1));
-    levelTF = new TextField("level");
+    levelTF = new TextField();
     cajaTS = new RadioButton("Caja");
-
-    TilePane tile = new TilePane(Orientation.VERTICAL);
-    tile.setPadding(new Insets(5, 0, 5, 0));
-    tile.setVgap(4);
-    tile.setHgap(4);
-    tile.setPrefColumns(2);
-    tile.setStyle("-fx-background-color: DAE6F3;");
-    tile.setAlignment(Pos.BASELINE_CENTER);
-
-    tile.getChildren().add(nameTF);
-    tile.getChildren().add(empleoCh);
-    tile.getChildren().add(levelTF);
-    tile.getChildren().add(fijoCh);
-    tile.getChildren().add(temporal);
-    tile.getChildren().add(cajaTS);
-    tile.getChildren().add(matutinoCh);
-    tile.getChildren().add(vespertinoCh);
-    tile.getChildren().add(fechaContrataciónDP);
-    tile.getChildren().add(contratoCh);
-    tile.getChildren().add(btn);
-
-    Scene scene = new Scene(tile, 300, 500);
-
-    primaryStage.setTitle("Añadir Condesos");
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    errorMssg = new Label();
   }
+
   public static void main(String[] args) {
     launch(args);
   }
