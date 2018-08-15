191ef705cda8d4b4f09ec69f9e78f48e4c624149
SoftProductsDev <softproductsdev@gmail.com>
2018-08-15 23:33:06 +0200

    pagina de inicio

--- src/main/java/condesaGUI/MainGUI.java
+++ src/main/java/condesaGUI/MainGUI.java
@@ -1,7 +1,14 @@
 package condesaGUI;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
@@ -12,17 +19,65 @@ public class MainGUI extends Application {
     private Node[] controls;
     @Override
     public void start(Stage primaryStage) throws Exception {
+        CreateLayout();
+        CreateControls();
+        AddControlsToLayout();
+        Scene scene = new Scene(Tile, 800, 500);
+        primaryStage.setTitle("Horarios CONDESA");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     private void AddControlsToLayout()
     {
+        for (int i=0; i < 3; i++)
+        {
+            Tile.getChildren().add(controls[i]);
+        }
     }
 
     private void CreateControls()
     {
+        controls = new Node[3];
+        Button algoritmo = new Button();
+        algoritmo.setText("Nuevo Horario");
+        algoritmo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+//TODO
+            }
+        });
+        Button tiendas = new Button();
+        tiendas.setText("Tiendas");
+        tiendas.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                AddTiendaGUI tiendas = new AddTiendaGUI();
+//TODO
+            }
+        });
+        Button condesos = new Button();
+        condesos.setText("Condesos");
+        condesos.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                AddCondesoGUI condesos = new AddCondesoGUI();
+//TODO
+            }
+        });
+        controls[0] = condesos;
+        controls[1] = tiendas;
+        controls[2] = algoritmo;
     }
 
     private void CreateLayout()
     {
+        Tile = new TilePane(Orientation.HORIZONTAL);
+        Tile.setPadding(new Insets(5, 5, 5, 5));
+        Tile.setVgap(3);
+        Tile.setHgap(5);
+        Tile.setPrefColumns(2);
+        Tile.setStyle("-fx-background-color: #ff63b4;");
+        Tile.setAlignment(Pos.TOP_LEFT);
     }
 }
