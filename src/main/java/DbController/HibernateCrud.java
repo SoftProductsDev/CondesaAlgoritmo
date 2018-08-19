65b20a75d164414d5029ecca4b7b4e9f3c8226d1
javier <javier@FVACAGTO>
2018-08-18 19:44:58 -0500

    Crud completo, no tests

--- src/main/java/DbController/HibernateCrud.java
+++ src/main/java/DbController/HibernateCrud.java
@@ -2,6 +2,8 @@ package DbController;
 
 import DbModel.Condeso;
 import DbModel.HibernateUtil;
+import java.util.List;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -42,6 +44,55 @@ public class HibernateCrud {
 
     HibernateUtil.shutdown();
     return ("condeso saved, id:  " + condesodb.getId());
+  }
+
+  public static String UpdateCondeso(Condeso updatedCondeso){
+    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    Session session = sessionFactory.openSession();
+
+    session.getTransaction().begin();
+    session.update(updatedCondeso);
+    session.getTransaction().commit();
+
+    session.close();
+    HibernateUtil.shutdown();
+
+    return "Updated condeso: " + updatedCondeso.toString();
+  }
+
+  public static String DeleteCondeso(Condeso deletedCondeso) {
+    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    Session session = sessionFactory.openSession();
+
+    session.getTransaction().begin();
+    session.delete(deletedCondeso);
+    session.getTransaction().commit();
+    session.close();
+    HibernateUtil.shutdown();
+
+    return  "Deleted:" + deletedCondeso.toString();
+  }
+
+  public static Condeso GetCondeso(Condeso condesoBuscado){
+    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    Session session =  sessionFactory.openSession();
+    Condeso condeso =  (Condeso) session.get(Condeso.class, condesoBuscado.getId());
+    session.close();
+    HibernateUtil.shutdown();
+    return condeso;
+  }
+
+  public static List<Condeso> GetAllCondesos(){
+    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    Session session = sessionFactory.openSession();
+
+    Criteria criteria = session.createCriteria(Condeso.class);
+    List<Condeso> condesos = criteria.list();
+
+    session.close();
+    HibernateUtil.shutdown();
+
+    return condesos;
   }
 
 }
