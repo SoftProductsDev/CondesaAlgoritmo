6b79d815b3deb46d063f901b1084eb691a09ea53
Sebastián Galindez Tapia <sebgal2008@gmail.com>
2018-08-19 18:41:53 +0200

    ya está el parser, pero falta debuggear

--- src/main/java/lalo/Parser.java
+++ src/main/java/lalo/Parser.java
@@ -8,15 +8,55 @@ import java.io.IOException;
 public class Parser {
     public static int[][] parse(String fileName) {
         String line;
-
+        String month = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28";
+        String number;
+        int hour;
+        int[][] disponibilidad;
         try {
             FileReader reader = new FileReader(fileName);
             BufferedReader buffer = new BufferedReader(reader);
 
             while ((line = buffer.readLine()) != null) {
-                int i = ignore(line, ' ');
-                if(i < line.length()){
-                    line.
+                if(line.contains(month)){
+                    int i = ignore(line, ' ');
+                    i += 75;
+                    int j = 0;
+                    while(line.charAt(i) != ' '){
+                        i+=3;
+                        j++;
+                    }
+                 disponibilidad = new int[3][28+j];
+
+                    for(int k = 0; k < disponibilidad[0].length){
+                        disponibilidad[0][k] = k+1;
+                    }
+
+                 line = buffer.readLine();
+                 i = 0;
+                 i = ignore(line, ' ');
+                 for(int k = 0; k < disponibilidad[1].length; k++){
+                 number = line.substring(i, (i = subString(line, i, ' ' )));
+                 try{
+                    hour = Integer.parseInt(number);
+                 } catch(Exception e){
+                     hour = 0;
+                 }
+                 disponibilidad[1][k] = hour;
+                 }
+                 line = buffer.readLine();
+                    i = 0;
+                    i = ignore(line, ' ');
+                    for(int k = 0; k < disponibilidad[2].length; k++){
+                        number = line.substring(i, (i = subString(line, i, ' ' )));
+                        try{
+                            hour = Integer.parseInt(number);
+                        } catch(Exception e){
+                            hour = 0;
+                        }
+                        disponibilidad[2][k] = hour;
+                    }
+
+
                 }
 
             }
@@ -41,6 +81,14 @@ private static int ignore(String input, char toIgnore){
             i++;
         }
         return i;
+}
+
+private static int subString(String input, int i, char last){
+        int j = i + 1;
+        while(input.charAt(j) != last){
+            int j = j++;
+        }
+        return j;
 }
 
 }
