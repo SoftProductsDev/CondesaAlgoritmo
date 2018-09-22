ed2c93bd535ca923b5a3d65db1e525da35c999be
Sebastián Galindez Tapia <sebgal2008@gmail.com>
2018-09-22 02:20:12 -0500

    primera pasada de lalo casi lista

--- src/main/java/lalo/lalo.java
+++ src/main/java/lalo/lalo.java
@@ -17,20 +17,13 @@ import horario.HorarioEntrega;
 
 public class lalo {
 	public Set<HorarioEntrega> entregas;
-	public Queue<Condeso> condesos;
+	public Set<Condeso> condesos;
 	public Set<Tiendas> tiendas;
 	private PriorityQueue<Turnos> turnos;
 
 	private PriorityQueue<Condeso> fila;
 	private HashMap<Integer, Integer[][]> disponibilidad;
 
-
-
-	/*private void ListaDeTurnos(){
-		for(Tiendas t : tiendas){
-			
-		}
-	}*/
 
 	public void  GMTodos() {
 		//TODO
@@ -47,26 +40,46 @@ public class lalo {
 	public void pocoONadaDeJuego() {
 		//TODO
 	}
+
 	public void laloFuncionando() {
 		Set<Condeso> noDisponible = new HashSet<>();
 		Set<Turnos> noAsignados = new HashSet<>();
+		Set<Condeso> yaOcupados = new HashSet<>();
 
 		Turnos elTurno = turnos.poll();
+		Turnos last;
 		while(elTurno != null){
-			Condeso elCondeso = fila.poll();
-			while(!checkCondeso(elCondeso, disponibilidad, elTurno)){
-				noDisponible.add(elCondeso);
-				elCondeso = fila.poll();
+			last = elTurno;
+			while(last.getDate().getDayOfMonth() == elTurno.getDate().getDayOfMonth()) {
+				Condeso elCondeso = fila.poll();
+				while (!checkCondeso(elCondeso, disponibilidad, elTurno)) {
+					noDisponible.add(elCondeso);
+					elCondeso = fila.poll();
+				}
+				if (elCondeso == null) noAsignados.add(elTurno);
+				else{
+					AsignarTurno(elCondeso, elTurno);
+					yaOcupados.add(elCondeso);
+				}
+				last = elTurno;
+				elTurno = turnos.poll();
+				fila.addAll(noDisponible);
+				noDisponible.clear();
 			}
-			if(elCondeso == null) noAsignados.add(elTurno);
-			else AsignarTurno(elCondeso, elTurno);
-
+			fila.addAll(yaOcupados);
+			yaOcupados.clear();
 		}
+
+		reacomodar(noAsignados, condesos, disponibilidad);
+
 	}
+
 	private boolean checkCondeso(Condeso elCondeso, HashMap<Integer, Integer[][]> disponibilidad, Turnos elTurno){
+		if(elCondeso == null) return true;
 		if(!checkDisponibilidad(elCondeso, disponibilidad, elTurno)) return false;
 		if(!checkDiasSeguidos(elCondeso, elTurno)) return false;
 		if(!checkFinesLibres(elCondeso, elTurno)) return false;
+		if(!checkMax(elCondeso, elTurno)) return false;
 		return true;
 
 	}
@@ -85,6 +98,7 @@ public class lalo {
 	}
 
 	private boolean checkDiasSeguidos(Condeso elCondeso, Turnos elTurno){
+		//TODO
 		return false;
 	}
 
@@ -96,8 +110,16 @@ public class lalo {
 		return true;
 	}
 
-	private void AsignarTurno(Condeso elCondeso, Turnos elTurno){
+	private boolean checkMax(Condeso elCondeso, Turnos elTurnos){
+		return false;
+	}
 
+	private void AsignarTurno(Condeso elCondeso, Turnos elTurno){
+		//TODO
+	}
+
+	private void reacomodar(Set<Turnos> noAsignados, Set<Condeso> condesos, HashMap<Integer, Integer[][]> disponibilidad){
+		//TODO
 	}
 
 
