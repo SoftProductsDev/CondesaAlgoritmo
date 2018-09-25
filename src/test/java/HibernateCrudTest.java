4e8749fdb6e1586021fc8b6eb200488946432c0a
SoftProductsDev <rikysvera@gmail.com>
2018-09-25 21:45:11 +0200

    condesos y tiendas agregados

--- src/test/java/HibernateCrudTest.java
+++ src/test/java/HibernateCrudTest.java
@@ -24,35 +24,242 @@ public class HibernateCrudTest extends TestCase {
   }
 
   public void testCreateCondeso(){
-    HashMap<LocalDate, Dias> entrega = new HashMap<LocalDate, Dias>();
-    Dias dia = new Dias();
-    LocalDate date = LocalDate.now();
-    dia.setDate(date);
+    List<DbModel.Tiendas> tiendas = new ArrayList<>();
+    DbModel.Tiendas hbf = new DbModel.Tiendas();
+    hbf.setNombre("HBF");
+    hbf.setManager("Jorge");
+    hbf.setFechaApertura(LocalDate.now());
+    hbf.setId(1);
 
-    HashMap<Integer, Turnos> turnos = createTurnos();
-    //dia.setTurnos(turnos);
 
-    entrega.put(dia.getDate(), dia);
+    DbModel.Tiendas mf = new DbModel.Tiendas();
+    mf.setNombre("MF");
+    hbf.setManager("Leo");
+    hbf.setFechaApertura(LocalDate.now());
+    hbf.setId(2);
+
+
+    DbModel.Tiendas impler = new DbModel.Tiendas();
+    hbf.setNombre("IMP");
+    hbf.setManager("Jorge");
+    hbf.setFechaApertura(LocalDate.now());
+    hbf.setId(3);
+
+
+    tiendas.add(impler);
+    tiendas.add(mf);
+    tiendas.add(hbf);
+
 
     Condeso condeso = new Condeso();
     condeso.setCaja(true);
     condeso.setAntiguedad(LocalDate.now());
     condeso.setContrato(Contrato.Tipo1);
-    //condeso.setEntrega(entrega);
     condeso.setFijos(true);
     condeso.setManana(true);
     condeso.setTarde(true);
     condeso.setLevel(1);
     condeso.setTipo(TipoEmpleado.Encargado);
-    condeso.setNombre("Empleado");
-    List<DbModel.Tiendas> tiendas = new ArrayList<>();
-    DbModel.Tiendas tienda = new DbModel.Tiendas();
-    tienda.setNombre("HBF");
-    tienda.setId(1);
-    tiendas.add(tienda);
+    condeso.setNombre("Pepe");
+    condeso.setColor("#f48642");
     condeso.setDondePuedeTrabajar(tiendas);
-
     DbController.HibernateCrud.SaveCondeso(condeso);
+
+    Condeso condeso2 = new Condeso();
+    condeso.setCaja(false);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo2);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(2);
+    condeso.setTipo(TipoEmpleado.Equipo);
+    condeso.setNombre("Pablo");
+    condeso.setColor("#f1f441");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso2);
+
+    Condeso condeso3 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo3);
+    condeso.setFijos(false);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(3);
+    condeso.setTipo(TipoEmpleado.GM);
+    condeso.setNombre("Javier");
+    condeso.setColor("#f44141");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso3);
+
+    Condeso condeso4 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Sofia");
+    condeso.setColor("#be41f4");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso4);
+
+    Condeso condeso5 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Nuevo);
+    condeso.setNombre("Ximena");
+    condeso.setColor("#7f41f4");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso5);
+
+    Condeso condeso6 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Nuevo);
+    condeso.setNombre("Esteban");
+    condeso.setColor("#61f441");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso6);
+
+    Condeso condeso7 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Jose");
+    condeso.setColor("#41f4ee");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso7);
+
+    Condeso condeso8 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Memo");
+    condeso.setColor("#4c41f4");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso8);
+
+    Condeso condeso9 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Tonio");
+    condeso.setColor("#f4b841");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso9);
+
+    Condeso condeso10 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Abraham");
+    condeso.setColor("#f47041");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso10);
+
+    Condeso condeso11 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Martin");
+    condeso.setColor("#0d5f63");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso11);
+
+    Condeso condeso12 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Pepe");
+    condeso.setColor("#750202");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso12);
+
+    Condeso condeso13 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Esther");
+    condeso.setColor("#6c0c6d");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso13);
+
+    Condeso condeso14 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Jose");
+    condeso.setColor("#b7bdf7");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso14);
+
+    Condeso condeso15 = new Condeso();
+    condeso.setCaja(true);
+    condeso.setAntiguedad(LocalDate.now());
+    condeso.setContrato(Contrato.Tipo1);
+    condeso.setFijos(true);
+    condeso.setManana(true);
+    condeso.setTarde(true);
+    condeso.setLevel(1);
+    condeso.setTipo(TipoEmpleado.Encargado);
+    condeso.setNombre("Dariana");
+    condeso.setColor("#b7f7ed");
+    condeso.setDondePuedeTrabajar(tiendas);
+    DbController.HibernateCrud.SaveCondeso(condeso15);
   }
 
   private HashMap<Integer, Turnos> createTurnos() {
