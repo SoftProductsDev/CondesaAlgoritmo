c80bad99902429d1eb661298c78de23941169790
Sebastián Galindez Tapia <sebgal2008@gmail.com>
2018-10-19 15:59:08 +0200

    nuevo compare Turnos

---
+++ src/main/java/horario/CompareTurnos2.java
@@ -0,0 +1,15 @@
+package horario;
+
+import java.util.Comparator;
+
+public class CompareTurnos2 implements Comparator<Turnos> {
+
+
+    @Override
+    public int compare(Turnos o1, Turnos o2) {
+        if(o1.getInicio() != o2.getInicio()) return  Integer.compare(o1.getInicio(), o2.getInicio());
+        else{
+          return o1.getTipoTurno().compareTo(o2.getTipoTurno());
+        }
+    }
+}
