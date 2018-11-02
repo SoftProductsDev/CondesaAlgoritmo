b7e6f7a34f56849e33332be60328b877222807dd
javier <javiervaca96@gmail.com>
2018-11-01 22:08:45 -0600

    Excel 666

--- src/main/java/ExcelController/ExcelWriter.java
+++ src/main/java/ExcelController/ExcelWriter.java
@@ -1,7 +1,12 @@
 package ExcelController;
 
+import horario.HorarioMaster;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
@@ -13,6 +18,7 @@ public class  ExcelWriter {
   private static String[] columns = {"GM", "GM","G", "F", "D", "B", "R"};
   private static CellStyle borders;
   private static Workbook workbook;
+  private Sheet currentSheet;
 
   public ExcelWriter(){
     workbook = createWorkbook();
@@ -39,49 +45,37 @@ public class  ExcelWriter {
     return borders;
   }
 
-  public void CreateHorarioMasterExcel() throws IOException, InvalidFormatException {
+  public void CreateHorarioMasterExcel(String nombreTienda, HorarioMaster horarioMaster) throws IOException, InvalidFormatException {
 
     CreationHelper createHelper = workbook.getCreationHelper();
 
     // Create a Sheet
     Sheet sheet = workbook.createSheet("HorarioMaster");
+    currentSheet = sheet;
 
-
+    createHourList(sheet);
 
     // Create a Row
     Row lettersRow = sheet.createRow(4);
+    Row dayOfMonthRow = sheet.createRow(3);
+    Row dayOfWeekRow = sheet.createRow(2);
 
-    // Create cells
-    for(int i = 0; i < columns.length; i++) {
-      Cell cell = lettersRow.createCell(i + 1);
-      cell.setCellValue(columns[i]);
-      cell.setCellStyle(borders);
+    for(int i = 1; i < 100; i+=8){
+      setLettersUP(sheet, i, lettersRow);
+      setDayOfWeek(DayOfWeek.SATURDAY, sheet, i,dayOfWeekRow);
+      setDayOfMonth(LocalDate.now(), sheet, i, dayOfMonthRow);
+      setTurnosRegion(sheet, i);
     }
 
-    Row row = sheet.createRow(2);
-    Cell cell = row.createCell(1);
-    cell.setCellValue("Mo");
+    /*setLettersUP(sheet, 9, lettersRow);
+    setDayOfWeek(DayOfWeek.SUNDAY, sheet, 9, dayOfWeekRow);
+    setDayOfMonth(LocalDate.now(), sheet, 9, dayOfMonthRow);
+    setTurnosRegion(sheet, 9);
 
-    sheet.addMergedRegion(new CellRangeAddress(
-        2, //first row (0-based)
-        2, //last row  (0-based)
-        1, //first column (0-based)
-        7  //last column  (0-based)
-    ));
-
-    sheet.addMergedRegion(new CellRangeAddress(
-        3, //first row (0-based)
-        3, //last row  (0-based)
-        1, //first column (0-based)
-        7  //last column  (0-based)
-    ));
-
-    CellRangeAddress turnosRegion = new CellRangeAddress(
-        5,17,1,7
-    );
-
-    createHourList(sheet);
-    setRegionBorderWithMedium(turnosRegion, sheet);
+    setLettersUP(sheet, 17, lettersRow);
+    setDayOfWeek(DayOfWeek.MONDAY, sheet, 17, dayOfWeekRow);
+    setDayOfMonth(LocalDate.now(), sheet, 17, dayOfMonthRow);
+    setTurnosRegion(sheet, 17);*/
 
     // Write the output to a file
     //Tambien puede especificar el path ("C:\\Report\\TestCase.xlsx"));
@@ -92,6 +86,47 @@ public class  ExcelWriter {
     // Closing the workbook
     workbook.close();
   }
+
+  private void setDayOfMonth(LocalDate date, Sheet sheet, int column, Row row) {
+    Cell cell = row.createCell(column);
+    String day =  Integer.toString(date.getDayOfMonth());
+    cell.setCellValue(day);
+    CellRangeAddress regionDia =  new CellRangeAddress(
+        3, //first row (0-based)
+        3, //last row  (0-based)
+        column, //first column (0-based)
+        column + 6 ); //last column  (0-based)
+    //Unifica las celdas
+    sheet.addMergedRegion(regionDia);
+    //Les pone margen
+    setRegionBorderWithMedium(regionDia, sheet);
+  }
+
+  private void setLettersUP(Sheet sheet, int column, Row row){
+
+    // Create cells
+    for(int i = 0; i < columns.length; i++) {
+      Cell cell = row.createCell(i + column);
+      cell.setCellValue(columns[i]);
+      cell.setCellStyle(borders);
+      sheet.setColumnWidth(i + column, 800);
+    }
+  }
+
+ private void setDayOfWeek(DayOfWeek dayOfWeek,Sheet sheet, int column, Row row){
+   Cell cell = row.createCell(column);
+   String day =  dayOfWeek.getDisplayName(TextStyle.FULL, Locale.GERMAN);
+   cell.setCellValue(day);
+   CellRangeAddress regionDia =  new CellRangeAddress(
+       2, //first row (0-based)
+       2, //last row  (0-based)
+       column, //first column (0-based)
+       column + 6 ); //last column  (0-based)
+   //Unifica las celdas
+   sheet.addMergedRegion(regionDia);
+   //Les pone margen
+   setRegionBorderWithMedium(regionDia, sheet);
+ }
 
   private void createHourList(Sheet sheet) {
     String[] rows = {"09-10", "10-11","11-12", "12-13", "13-14", "14-15", "15-16", "16-17",
@@ -114,9 +149,16 @@ public class  ExcelWriter {
     RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
   }
 
+  private void setTurnosRegion(Sheet sheet, int column){
+    CellRangeAddress turnosRegion = new CellRangeAddress(
+        5,17,column,column + 6
+    );
+    setRegionBorderWithMedium(turnosRegion, sheet);
+  }
+
   public static void main(String[] args) throws IOException, InvalidFormatException{
     ExcelWriter excelWriter = new ExcelWriter();
-    excelWriter.CreateHorarioMasterExcel();
+    excelWriter.CreateHorarioMasterExcel("HBF", new HorarioMaster());
   }
 
 }
